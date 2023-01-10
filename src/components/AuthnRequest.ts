b61a676a84b8eef07707cbb2cc25ae04a99fca2b
Teo Wong <teo.wong@eastech.com.hk>
2023-01-10 18:10:50 +0800

    Init Project

---
+++ src/components/AuthnRequest.ts
@@ -0,0 +1,52 @@
+import { XMLAttribute, XMLChild, XMLElement } from 'xml-decorators';
+
+const HOBBY_NS = 'h';
+
+class Hobby {
+
+  @XMLAttribute({ namespace: HOBBY_NS })
+  private name: string;
+
+  @XMLAttribute({ namespace: HOBBY_NS })
+  private description: string;
+}
+
+const PERSON_ROOT = 'person';
+const PERSON_NS = 'ps';
+
+@XMLElement({ root: PERSON_ROOT }) // optional
+class Person {
+
+  @XMLAttribute({ namespace: PERSON_NS })
+  private firstname: string;
+
+  private lastname: string;
+
+  @XMLAttribute({ namespace: PERSON_NS })
+  get fullname(): string {
+
+    return this.firstname + ' ' + this.lastname;
+  }
+
+  @XMLAttribute({ namespace: PERSON_NS })
+  private age: number;
+
+  @XMLChild({
+    namespace: PERSON_NS,
+    name: 'hobby'
+  })
+  private hobbies: Hobby[];
+
+  @XMLChild({
+    namespace: PERSON_NS,
+    stripPluralS: true
+  })
+  private friends: Person[];
+
+  @XMLChild({
+    name: 'pet',
+    implicitStructure: 'pets.$'
+  })
+  private pets: string[];
+
+}
